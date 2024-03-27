73f06fb0c1d4bdd52f0065d1bdc9c8375ca70186
ali tarichi <tarichiali@gmail.com>
2024-03-27 16:04:11 +0300

    works scraping the gardian website for climate news in real time

--- climate-change-api/index.js
+++ climate-change-api/index.js
@@ -5,8 +5,28 @@ const cheerio = require('cheerio')
 
 const app = express()
 
+
+
+const articles = []
+
 app.get('/', (req, res) =>{
     res.json("welcome to my Climet News API")
+})
+
+app.get('/news/', (req, res) => {
+    axios.get('https://www.theguardian.com/environment/climate-crisis')
+        .then((response) => {
+            const html = response.data
+            console.log(html)
+            const $ = cheerio.load(html)
+
+            $('a:contains("climate")',html).each(function (){
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                articles.push({title, url})
+            })
+            res.json(articles)
+    })  .catch((err) => console.log(err))
 })
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
